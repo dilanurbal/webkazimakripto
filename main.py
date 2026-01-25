e204d9323cdbdbce39da315c7f3391728c97f262
Dilanur Bal <dilanurball@gmail.com>
2026-01-25 15:18:43 +0300

    README, requirements ve esnek coin çekme fonksiyonu eklendi

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from kripto import fiyatlari_cek
 from Veriler import kaydet_json
 
-
 def main():
     print("Veriler çekiliyor...")
-    fiyatlar = fiyatlari_cek()
+
+    fiyatlar = {
+        "bitcoin": fiyatlari_cek(coin_adi="Bitcoin"),
+        "ethereum": fiyatlari_cek(coin_adi="Ethereum"),
+        "bnb": fiyatlari_cek(coin_adi="BNB")
+    }
+
     print("Fiyatlar alındı:", fiyatlar)
 
     print("JSON dosyasına kaydediliyor...")
     kaydet_json(fiyatlar)
-
 
 if __name__ == '__main__':
     main()
